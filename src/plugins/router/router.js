966511688ae9fe775a98006e5e8d7ff9bfca8abb
Owen Sawler <owensawler4@gmail.com>
2023-08-28 13:56:27 -0400

    removed about due to build error

--- src/plugins/router/router.js
+++ src/plugins/router/router.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/about.vue'
 
 const routes = [
     { path: '', name: 'home', component: Home },
-    { path: '/about', name: 'about', component: About },
 ]
 
 const router = createRouter({
