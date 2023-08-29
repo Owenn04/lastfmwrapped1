72dfe0dee9efe6118df42efa0de528fef4f2dffc
Owen Sawler <owensawler4@gmail.com>
2023-08-28 23:43:20 -0400

    last att before sleep

--- netlify/functions/get-session.js
+++ netlify/functions/get-session.js
@@ -1,29 +1,38 @@
-const axios = require('axios');
 const CryptoJS = require("crypto-js");
+const axios = require("axios");
 
 exports.handler = async (event, context) => {
-  try {
     const { token } = event.queryStringParameters;
     const apiKey = process.env.VITE_API_KEY;
     const method = 'auth.getSession';
-    const sharedSecret = process.env.VITE_SHARED_SECRET
-    const concatenatedString = apiKey + method + token + sharedSecret;
-    const apiSig = CryptoJS.MD5(concatenatedString).toString();
-    
-    const endpoint = `http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${apiKey}&token=${token}&api_sig=${apiSig}&format=json`;
-    const response = await axios.get(endpoint);
-    const sessionInfo = response.data
-    const username = sessionInfo.session.key
+    const sharedSecret = process.env.VITE_SHARED_SECRET;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ username }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'An error occurred' }),
-    };
-  }
+    // Concatenate parameters and shared secret
+    const concatenatedString = apiKey + "method" + method + "token" + token + sharedSecret;
+
+    // Generate MD5 hash
+    const apiSig = CryptoJS.MD5(concatenatedString).toString();
+
+    // Construct the API endpoint
+    const endpoint = `http://ws.audioscrobbler.com/2.0/?method=${method}&api_key=${apiKey}&token=${token}&api_sig=${apiSig}&format=json`;
+
+    try {
+        // Make API request
+        const response = await axios.get(endpoint);
+        const sessionInfo = response.data;
+        const username = sessionInfo.session.key;
+
+        // Handle the response or perform further operations
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ username }),
+        };
+    } catch (error) {
+        // Handle errors
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "An error occurred." }),
+        };
+    }
 };
 
