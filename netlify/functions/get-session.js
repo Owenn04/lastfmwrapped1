9f451a47cb497d40e8b5213774c1cb1c35efd0eb
Owen Sawler <owensawler4@gmail.com>
2023-08-30 18:13:50 -0400

    test build

--- netlify/functions/get-session.js
+++ netlify/functions/get-session.js
@@ -5,10 +5,12 @@ const CryptoJS = require("crypto-js");
 exports.handler = async (event, context) => {
     const { token } = event.queryStringParameters;
     const apiKey = process.env.VITE_API_KEY;
+    console.log(apiKey)
     const method = 'auth.getSession';
     const sharedSecret = process.env.VITE_SHARED_SECRET;
 
-    const concatenatedString = `api_key${apiKey}method${method}token${token}${sharedSecret}`;
+    // const concatenatedString = `api_key${apiKey}method${method}token${token}${sharedSecret}`;
+    const concatenatedString = `api_keybd6bf9516e5039a9837aeeb96f2c4a9cmethod${method}token${token}2ad9a87d606fa9662e5329542dc44366`;
     const apiSig = createHash('md5').update(concatenatedString, 'utf-8').digest('hex');
     const baseURL = 'https://ws.audioscrobbler.com/2.0';
 
